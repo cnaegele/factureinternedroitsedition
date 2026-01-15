4092be807d0a17118bf2e4bc2b2219e1ff03c397
cnaegele <cnaegele@lausanne.ch>
2026-01-15 17:34:23 +0100

    facture interne droit edition service

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+
+<template>
+<header class="header-flex">
+  <h2>Gestion hors comptabilité de direction des droits  d'édition des factures internes par service.</h2>
+  <div class="user-info">
+    <suspense>
+      <UserInformation groupeSecurite="GoelandManager"></UserInformation>
+    </suspense>
+  </div>
+</header>
+  <main>
+    <div id="app">
+      <div v-if="lesDatas.messageErreur != ''" id="divErreur">{{ lesDatas.messageErreur }}</div>
+      <div v-else class="flex-container">
+        <div><suspense><DirectionService></DirectionService></suspense></div>
+        <div><suspense><EmployeEdition></EmployeEdition></suspense></div>
+        <div><suspense><EmployeChoix></EmployeChoix></suspense></div>
+      </div>
+
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { useDataStore } from '@/stores/datastore.ts'
+import UserInformation from '@/components/UserInformation.vue'  
+import DirectionService from './components/DirectionService.vue'
+import EmployeEdition from './components/EmployeEdition.vue'
+import EmployeChoix from './components/EmployeChoix.vue'
+
+const lesDatas = useDataStore()
+</script>
+
+<style scoped>
+.header-flex {
+  display: flex;
+  align-items: baseline;
+  gap: 15px;
+}
+
+.header-flex h2 {
+  margin: 0;
+  font-size: 1.5em;
+}
+
+.header-flex .user-info {
+  font-size: 1em;
+  color: #666;
+  margin: 0;
+}
+
+#divErreur{
+  background-color:lightsalmon;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-top: 0px;
+  padding: 5px;
+  border-style: solid;
+  border-width: thin;
+  border-color: black;
+  border-radius: 20px;
+}
+</style>
---
+++ src/axioscalls.ts
@@ -0,0 +1,172 @@
+import axios from 'axios'
+import type { AxiosResponse, AxiosError } from 'axios'
+import type { Employe } from '@/stores/datastore.ts'
+
+let server: string = ''
+if (import.meta.env.DEV) {
+    server = 'https://mygolux.lausanne.ch'    
+}
+
+export interface Utilisateur {
+  id_employe?: number
+  nom_employe?: string
+  prenom_employe?: string
+  login_employe?: string
+  groupesecurite?: string
+  bingroupe?: number
+  message?: string
+}
+export interface Service {
+  iduo: number
+  nomuo: string
+  descriptionuo: string
+  orderliste: string
+  orderlisteparent: string
+}
+export interface Direction {
+  iduo: number
+  nomuo: string
+  descriptionuo: string
+  orderliste: string
+  services: Service[]
+}
+export interface EmployeChoix {
+  idemploye: number
+  nom: string
+  prenom: string
+  bactif: number
+  login: string
+  unite: string
+  directionabr?: string
+  serviceabr?: string
+}
+export interface ApiResponseDS {
+  success: boolean
+  message: string
+  data?: Direction[]
+}
+export interface ApiResponseEmpS {
+  success: boolean
+  message: string
+  data?: Employe[]
+}
+export interface ApiResponseMessage {
+  success: boolean
+  message: string
+}
+// Interface générique pour les réponses API
+export interface ApiResponse<T> {
+  success: boolean
+  message: string
+  data?: T[]
+}
+
+export async function getDataUserInfo(groupeSecurite: string): Promise<Utilisateur> {
+    const page: string = '/goeland/gestion_spec/g_login_f5.php'
+    const url: string = `${server}${page}`
+    const params = new URLSearchParams([['groupesecurite', groupeSecurite]])
+    const response: AxiosResponse<Utilisateur> = await axios.get(url, { params })
+    console.log(response.data)
+    return response.data
+}
+
+export async function getDirectionsServices(): Promise<ApiResponseDS> {
+    const page: string = '/goeland/uniteorg/axios/uniteorg_directions_services_liste.php'
+    const url: string = `${server}${page}`
+    try {
+        const response: AxiosResponse<Direction[]> = await axios.get(url)
+        const respData: ApiResponseDS= {
+            "success": true,
+            "message": `ok`,
+            "data": response.data
+        }
+        console.log(respData)
+        return respData
+    } catch (error) {
+        return traiteAxiosError(error as AxiosError)
+    }
+}
+
+export async function getEmployesEdition(idUO: number): Promise<ApiResponseEmpS> {
+    const page: string = '/goeland/gestion_spec/afffactureint_droitedition/axios/afffactureint_empedition_liste.php'
+    const url: string = `${server}${page}`
+    const params = new URLSearchParams([['iduniteorg', idUO.toString()]])
+    try {
+        const response: AxiosResponse<Employe[]> = await axios.get(url, { params })
+        const respData: ApiResponseEmpS= {
+            "success": true,
+            "message": `ok`,
+            "data": response.data
+        }
+        console.log(respData)
+        return respData
+    } catch (error) {
+        return traiteAxiosError(error as AxiosError)
+    }
+}
+
+export async function getEmployesListe(critere: string, bretInactif: boolean): Promise<EmployeChoix[]> {
+    const page: string = '/goeland/gestion_spec/afffactureint_droitedition/axios/employes_liste.php'
+    const url: string = `${server}${page}`
+    const params = new URLSearchParams([['scritere', critere], ['bretinactif', bretInactif.toString()]])
+    const response: AxiosResponse<EmployeChoix[]> = await axios.get(url, { params })
+    return response.data
+}
+
+export async function supprimeDroitEdition(idOrgunit: number, idEmploye: number): Promise<ApiResponseMessage> {
+    const odata = {
+        idorgunit: idOrgunit,
+        idemploye: idEmploye
+    }
+    const jdata: string = JSON.stringify(odata)
+    const page: string = '/goeland/gestion_spec/afffactureint_droitedition/axios/afffactureint_empedition_supprime.php'
+    const url: string = `${server}${page}`
+    try {
+        const response: ApiResponseMessage = await axios.post(url, jdata, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log(response)
+        return response
+    } catch (error) {
+        return traiteAxiosError(error as AxiosError)
+    }
+}
+
+export async function sauveDroitEdition(idOrgunit: number, idEmploye: number): Promise<ApiResponseMessage> {
+    const odata = {
+        idorgunit: idOrgunit,
+        idemploye: idEmploye
+    }
+    const jdata: string = JSON.stringify(odata)
+    const page: string = '/goeland/gestion_spec/afffactureint_droitedition/axios/afffactureint_empedition_sauve.php'
+    const url: string = `${server}${page}`
+    try {
+        const response: ApiResponseMessage = await axios.post(url, jdata, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log(response)
+        return response
+    } catch (error) {
+        return traiteAxiosError(error as AxiosError)
+    }
+}
+
+function traiteAxiosError<T>(error: AxiosError): ApiResponse<T> {
+  let msgErr: string = ''
+  if (error.response) {
+    msgErr = `${error.response.data}<br>${error.response.status}<br>${error.response.headers}`    
+  } else if (error.request.responseText) {
+    msgErr = error.request.responseText
+  } else {
+    msgErr = error.message
+  }
+  const respData: ApiResponse<T> = {
+    "success": false,
+    "message": `ERREUR. ${msgErr}`,
+  }
+  return respData
+}
